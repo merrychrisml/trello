4adbf3f94f2697380e916e9bc4a3aefaba7231a3
merrychrisml <wnsah0173@gmail.com>
2022-10-18 00:14:35 +0900

    mulit board proceed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
-import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import { DragDropContext } from "react-beautiful-dnd";
 import { useRecoilState } from "recoil";
 import { toDoState } from "./atoms";
-import DraggableCard from "./Components/DraggableCrad";
+import Board from "./Components/Board";
 
 const Wrapper = styled.div`
   display: flex;
-  max-width: 480px;
+  max-width: 680px;
   width: 100%;
   margin: 0 auto;
   justify-content: center;
@@ -17,15 +17,8 @@ const Wrapper = styled.div`
 const Boards = styled.div`
   display: grid;
   width: 100%;
-  grid-template-columns: repeat(1, 1fr);
-`;
-
-const Board = styled.div`
-  padding: 20px 10px;
-  padding-top: 30px;
-  background-color: ${(props) => props.theme.boardColor};
-  border-radius: 5px;
-  min-height: 200px;
+  gap: 10px;
+  grid-template-columns: repeat(3, 1fr);
 `;
 
 function App() {
@@ -52,16 +45,9 @@ function App() {
     <DragDropContext onDragEnd={onDragEnd}>
       <Wrapper>
         <Boards>
-          <Droppable droppableId="one">
-            {(magic) => (
-              <Board ref={magic.innerRef} {...magic.droppableProps}>
-                {toDos.map((toDo, index) => (
-                  <DraggableCard key={toDo} index={index} toDo={toDo} />
-                ))}
-                {magic.placeholder}
-              </Board>
-            )}
-          </Droppable>
+          {Object.keys(toDos).map((boardId) => (
+            <Board boardId={boardId} key={boardId} toDos={toDos[boardId]} />
+          ))}
         </Boards>
       </Wrapper>
     </DragDropContext>
---
+++ src/Components/Board.js
@@ -0,0 +1,28 @@
+import { Droppable } from "react-beautiful-dnd";
+import DraggableCard from "./DraggableCard";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  padding: 20px 10px;
+  padding-top: 30px;
+  background-color: ${(props) => props.theme.boardColor};
+  border-radius: 5px;
+  min-height: 200px;
+`;
+
+function Board({ toDos, boardId }) {
+  return (
+    <Droppable droppableId={boardId}>
+      {(magic) => (
+        <Wrapper ref={magic.innerRef} {...magic.droppableProps}>
+          {toDos.map((toDo, index) => (
+            <DraggableCard key={toDo} index={index} toDo={toDo} />
+          ))}
+          {magic.placeholder}
+        </Wrapper>
+      )}
+    </Droppable>
+  );
+}
+
+export default Board;
